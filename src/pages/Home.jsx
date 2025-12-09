213cfcfde9e3823dd5c3d989cf6d3ca87051d300
medié <danimedie@gmail.com>
2025-12-09 18:28:45 +0100

    home simple styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div className="flex items-center justify-center w-full min-h-[60vh]">
+      <section className="text-center p-8 space-y-3">
+        <h1 className="font-bold text-5xl">Welcome to <span className="text-accent">HAWKINS</span></h1>
+        <p>Explore the Stranger Things universe characters!</p>
+        <Link to='/characters' className="border bg-accent font-bold p-2 rounded-xl border-upsidedown text-upsidedown cursor-pointer hover:text-accent hover:border-accent hover:bg-upsidedown transition">View Characters</Link>
+      </section>
+    </div>
   )
 }
